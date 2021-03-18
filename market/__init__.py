bf38478a48aeb151d240529903931439d1572318
joe <joe94113@gmail.com>
2021-03-19 00:01:59 +0800

    create login

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:joe94113@127.0.0.1:3306/market'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "761f6c48a342735fc0ba7174"
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError  # 驗證套件
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="User Name：")
-    email_address = StringField(label="Email Address：")
-    password1 = PasswordField(label="Password：")
-    password2 = PasswordField(label="Confirm Password：")
-    submit = SubmitField(label="submit")
+
+    # FlaskForm 會尋找有關 validate_表格名稱 的函示執行驗證，所以才以這樣的方式命名
+    # FlaskForm理解將要進行驗證
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exists! Please try a different username")
+
+    def validate_email_address(self, email_address_checck):
+        email_address = User.query.filter_by(email_address=email_address_checck.data).first()
+        if email_address:
+            raise ValidationError("Email Address already exists! Please try a different email address")
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])  # 確認是否和密碼一致
+    submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label="User Name", validators=[DataRequired()])
+    password = StringField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label="Sign in")
